7676cedbbbcc4f8ba95a6ee4bb15e4002b4d4f21
Ayoub Lahdoud <ayoub.lahdoud@e-polytechnique.ma>
2025-04-20 14:13:42 +0100

    add PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,21 @@
+// next.config.js
+import withPWA from "next-pwa";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-      // ⛔ Ignore les erreurs ESLint pendant le build (utile pour Vercel)
-      ignoreDuringBuilds: true,
-    },
-  };
-  
-  export default nextConfig;
-  
+  eslint: {
+    // ⛔ Ignore les erreurs ESLint pendant le build (utile pour Vercel)
+    ignoreDuringBuilds: true,
+  },
+  reactStrictMode: true,
+};
+
+export default withPWA({
+  ...nextConfig,
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development", // désactive PWA en dev
+  },
+});
