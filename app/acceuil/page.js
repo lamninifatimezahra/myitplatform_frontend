4f93bb542d0eb76a6fe038210a3e645550bceb17
Ayoub Lahdoud <ayoub.lahdoud@e-polytechnique.ma>
2025-05-09 14:31:17 +0100

    Delete Parametres module

--- app/acceuil/page.js
+++ app/acceuil/page.js
@@ -86,12 +86,11 @@ export default function AccueilPage() {
 
   const modules = [
     { name: "Dashboard KPIs", path: "/dashboards", roles: ["admin", "user"] },
-    { name: "MyProfile", path: "/myprofile", roles: ["admin", "user"] },  // ✅ Ajouté ici
+    { name: "MyProfile", path: "/myprofile", roles: ["admin", "user"] },  
     { name: "MyForum", path: "/myforum", roles: ["admin", "user"] },
     { name: "MyAI", path: "/myai", roles: ["admin", "user"] },
     { name: "MyFile", path: "/myfile", roles: ["admin", "user"] },
     { name: "Guide MyIT", path: "/guide", roles: ["admin", "user"] },
-    { name: "Paramètres", path: "/settings", roles: ["admin", "user"] },
     { name: "MyPropos", path: "/mypropos", roles: ["admin", "user"] },
     { name: "Espace Admin", path: "/admin", roles: ["admin"] },
   ];
