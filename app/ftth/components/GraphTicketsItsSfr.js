aa3712ef4637de91773cc62425f275dc52ce5e50
Ayoub Lahdoud <ayoub.lahdoud@e-polytechnique.ma>
2025-07-09 10:07:56 +0100

    tickiting updatess

--- app/ftth/components/GraphTicketsItsSfr.js
+++ app/ftth/components/GraphTicketsItsSfr.js
@@ -43,8 +43,8 @@ export default function GraphTraitementTicketsITS_SFR() {
 
   const processData = () => {
     const raw = [
-      { name: "ITS", value: 112 },
-      { name: "SFR", value: 52 }
+      { name: "ITS", value: 121 },
+      { name: "SFR", value: 45 }
     ];
     setData(raw);
   };
